861c6ebd40173960d4d4a7b14eff96e454c3d24d
Компьютер <@DESKTOP-0TR3ASG>
2023-12-18 21:32:11 +0700

    Добавьте файлы проекта.

---
+++ 7.1/7.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+/*11 вариант. Написать функцию n(x), значение которой равно 0, 
+если x > 0, и единице во всех остальных случаях.
+Вычислить для чисел A, B, C
+n(A) + n(B) + n(C)*/
+int foo(int x) 
+{
+	int n;
+	if (x > 0) {
+		n = 0;
+	}
+	else
+	{
+		n = 1;
+	}
+	return n;
+}
+int main()
+{
+	int a, b, c;
+	std::cin >> a >> b >> c;
+	a=foo(a);
+	b=foo(b);
+	c=foo(c);
+	std::cout << a+b+c;
+
+}
